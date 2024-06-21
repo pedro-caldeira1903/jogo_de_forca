e9214805c49d9268d2782614a8f7d646e7fa528f
pedro-caldeira1903 <pedropaulocaldeira7@gmail.com>
2024-06-21 12:22:33 -0300

    Update ContaBancaria.java

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -3,17 +3,36 @@ public class abstract ContaBancaria {
   private int numero;
   private double saldo;
   public ContaBancaria(String senha, int numero, double saldo) {
-    this.senha = senha;
-    this.numero = numero;
-    this.saldo = saldo;
+    this.setSenha(senha);
+    this.setNumero(numero);
+    this.setSaldo(saldo);
   }
   public abstract void sacar(double valor);
   public abstract void depositar(double valor);
   public abstract void consultarExtrato();
-  public static void alteraSenha(String Novasenha) {
-    if (Novasenha != this.senha) {
-      this.senha = Novasenha;
-    }
-    System.out.println("A nova senha é "+ this.senha+"");
+  public void setSenha(String senha) {
+    this.senha = senha;
   }
-}
+  public String getSenha() {
+    return senha;
+  }
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+  public int getNumero() {
+    return numero;
+  }
+  public void setSaldo(double saldo) {
+    this.saldo = saldo;
+  }
+  public double getSaldo() {
+    return saldo;
+  }
+  public static void alterarSenha(String NovaSenha) {
+    Scanner sc=new Scanner(System.in);
+    String atualSenha = sc.next();
+    if (this.senha==atualSenha) {
+      this.senha = NovaSenha;
+    }
+    System.out.println("A sua senha "+atualSenha+" foi modificada para "+senha+"");
+  }
