e12fe4ab64bfba2c7954223d5b99bdf9578bd171
pedro-caldeira1903 <pedropaulocaldeira7@gmail.com>
2024-06-21 17:11:55 -0300

    Update ContaCorrente.java

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -3,21 +3,21 @@ public class ContaCorrente extends ContaBancaria {
   public ContaCorrente(String senha, int numero, double saldo, int transacoes) {
     super(senha, numero, saldo);
   }
-  public static void sacar(double valor){
-    double novoSaldo=saldo - (valor*0.25);
-    System.out.println("O valor que você vai sacar "+valor+" o saldo da conta vai ficar R$ "+novoSaldo+"");
-  }
-  public static void depositar(double valor){
-    double novoSaldo=saldo + valor;
-    System.out.println("O valor que você vai depositar "+valor+" o saldo da conta vai ficar R$ "+novoSaldo+"");
-  }
-  public static void consultarExtrato() {
-    System.out.println("O valor de transições são "+transacoes+"");
-  }
   public void setTransacoes(int transacoes) {
-    this.transacoes= transacoes;
+    this.transacoes = transacoes;
   }
   public int getTransacoes() {
     return transacoes;
+  }
+  public static void sacar(double valor) {
+    double novoSaldo=saldo - (valor * 0.25);
+    System.out.println("O valor que você sacou foi R$ "+valor+"e o saldo ficou R$ "+novoSaldo+".");
+  }
+  public static void depositar(double valor) {
+    double novoSaldo=saldo + (valor * 0.25);
+    System.out.println("O valor que você depositou foi R$ "+valor+"e o saldo ficou R$ "+novoSaldo+".");
+  }
+  public static void consultarExtrato() {
+    System.out.println("O número de transações são "+transacoes+".");
   }
 }
